031faaa14a8aa6ed91c39242dfa81479bd68b843
n-dev <123778887+n-dev6@users.noreply.github.com>
2023-09-13 12:15:12 +0200

    Ajout de commentaires
    Ajout de quelques commentaires. projet fini

--- server/app.js
+++ server/app.js
@@ -5,7 +5,8 @@ const bookRoutes = require('./routes/book');
 const userRoutes = require('./routes/user');
 const path = require('path');
 const mongoose = require('mongoose');
-  
+
+  // Configuration des en-têtes CORS
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
@@ -21,7 +22,8 @@ mongoose.connect('mongodb+srv://norybrakhlia:j1deS0ej8NeFA1YF@p7dev.a3szkt5.mong
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
-  
+
+// Utilisation des routes pour les livres, les utilisateurs et la gestion des images
 app.use('/api/books', bookRoutes);
 app.use('/api/auth', userRoutes);
 app.use('/images', express.static(path.join(__dirname, 'images')));
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
-const http = require('http');
-const app = require('./app');
+const http = require('http'); // Module HTTP de Node.js pour créer un serveur
+const app = require('./app'); // Notre application Express importée depuis app.js
 
+// Fonction pour normaliser le port
 const normalizePort = val => {
   const port = parseInt(val, 10);
   if (isNaN(port)) {
@@ -12,9 +13,10 @@ const normalizePort = val => {
   return false;
 };
 
-const port = normalizePort(process.env.PORT || '4000');
+const port = normalizePort(process.env.PORT || '4000'); // Port d'écoute par défaut : 4000
 app.set('port', port);
 
+// Gestion des erreurs liées au serveur
 const errorHandler = error => {
   if (error.syscall !== 'listen') {
     throw error;
@@ -35,13 +37,13 @@ const errorHandler = error => {
   }
 };
 
-const server = http.createServer(app);
+const server = http.createServer(app); // Création du serveur avec notre application Express
 
 server.on('error', errorHandler);
 server.on('listening', () => {
   const address = server.address();
   const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
-  console.log('Listening on ' + bind);
+  console.log('Listening on ' + bind); // Affichage du port d'écoute lorsque le serveur démarre
 });
 
-server.listen(port);
+server.listen(port); // Le serveur écoute le port spécifié
